7dbd630451a5064e9cbf16475a356ab27bd8f464
Alex <ue102151@edu.xunta.gal>
2021-02-17 17:29:31 +0100

    Commit 5 -desde Consola. Añadido un cambio

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 		System.out.println("¡Hola Mundo!");
 		System.out.println("Cambio 1 - Consola");
 		System.out.println("Cambio 1 - Eclipse");
+		System.out.println("Cambio 2 - Consola");
 	}
 
 } // Fin Clase HolaMundo
