13023c113be9aa95c4a55a862ea5c87e8c5ef71e
Alex <ue102151@edu.xunta.gal>
2021-02-17 17:17:59 +0100

    Commit 3 con 1 cambio desde Eclipse

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("¡Hola Mundo!");
 		System.out.println("Cambio 1 - Consola");
+		System.out.println("Cambio 1 - Eclipse");
 	}
 
 } // Fin Clase HolaMundo
